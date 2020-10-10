3255f8c81c1c65f65ad31a2c7d9cdfab15dd1ab6
Lokesh kumar <lokeshkumar2205@gmail.com>
2020-10-10 23:01:26 +0530

    Main class

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.Employee;
+
+public class Main {
+	public static void main(String args[])
+	{
+		Employee e1=new Employee();
+		e1.name="Saurabh";
+		e1.age=23;
+		e1.city="Chennai";
+		e1.display();
+		
+		Employee e2=new Employee();
+		e2.name="Lokesh";
+		e2.age=24;
+		e2.city="Hyderabad";
+		e2.display();
+		
+		
+	}
+
+}
